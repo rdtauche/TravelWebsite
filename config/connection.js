6a82c7931910ad7937bc56e70981379af6f3a041
Ross Tauchert <57552771+rdtauche@users.noreply.github.com>
2024-04-30 19:51:45 -0400

    connect to postgressql

---
+++ config/connection.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+
+const Sequelize = require('sequelize');
+
+    // Render code
+let sequelize;
+
+if (process.env.DB_URL) {
+  sequelize = new Sequelize(process.env.DB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PW,
+    {
+      host: 'localhost',
+      dialect: 'postgres',
+    },
+  );
+}
+
+module.exports = sequelize;
